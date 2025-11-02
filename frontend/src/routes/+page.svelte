7aa5575cf4fc1ecd3440a16a2727dfdc67ebf10a
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-11-01 17:44:55 -0700

    Add form component

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,5 +2,7 @@
 <p>An app designed to track your Qur'an progress.</p>
 <script>
     import Nav from "../components/Nav.svelte";
+    import Form from "../components/Form.svelte";
 </script>
 <Nav />
+<Form />
