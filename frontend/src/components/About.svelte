0541b2df37267d95dbf6cb45a07dd17a61dbcd53
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-11-01 17:57:21 -0700

    Format about component and add more stuff

--- frontend/src/components/About.svelte
+++ frontend/src/components/About.svelte
@@ -1,1 +1,5 @@
 <h1>About</h1>
+<p>
+  Quran Tracker is an app designed to track your Qur'an progress. Users are able
+  to record their current memorization, near review, and far review progress.
+</p>
