2c01cc8fe397726429f94eb278a217d17b3818a3
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-11-02 08:36:36 -0800

    Update sessions array when deleting a session

--- frontend/src/components/History.svelte
+++ frontend/src/components/History.svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import getAllQuranSessions from "../api/getAllQuranSessions";
+  import updateQuranSession from "../api/updateQuranSession";
+  import deleteQuranSession from "../api/deleteQuranSession";
 
   let sessions = $state<any>([]);
   onMount(async () => {
     sessions = await getAllQuranSessions();
   });
+
+  async function handleSessionUpdate(session: any) {
+    console.log("Update session:", session);
+    await updateQuranSession(
+      session.id,
+      session.session_type,
+      session.surah_num,
+      session.surah_name,
+      session.start_ayah,
+      session.end_ayah
+    );
+    console.log("Updated session:", session);
+  }
+
+  async function handleSessionDelete(session: any) {
+    console.log("Delete session:", session);
+    await deleteQuranSession(session.id);
+    sessions = sessions.filter((s: any) => s.id !== session.id);
+    console.log("Deleted session:", session);
+  }
 </script>
 
 <h1>History</h1>
@@ -19,4 +41,6 @@
     <p>{session.start_ayah}</p>
     <p>{session.end_ayah}</p>
   </div>
+  <button onclick={() => handleSessionUpdate(session)}>Update</button>
+  <button onclick={() => handleSessionDelete(session)}>Delete</button>
 {/each}
