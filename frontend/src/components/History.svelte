5699884f64478fb79b8475bf2362584beee3bb26
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-11-01 23:52:52 -0700

    Successfully get user history and render it on main pg

--- frontend/src/components/History.svelte
+++ frontend/src/components/History.svelte
@@ -1,1 +1,20 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import getAllQuranSessions from "../api/getAllQuranSessions";
+
+  let sessions = $state<any>([]);
+  onMount(async () => {
+    sessions = await getAllQuranSessions();
+  });
+</script>
+
 <h1>History</h1>
+{#each sessions as session}
+  <p>{session.id}</p>
+  <p>{session.createdAt}</p>
+  <p>{session.session_type}</p>
+  <p>{session.surah_num}</p>
+  <p>{session.surah_name}</p>
+  <p>{session.start_ayah}</p>
+  <p>{session.end_ayah}</p>
+{/each}
